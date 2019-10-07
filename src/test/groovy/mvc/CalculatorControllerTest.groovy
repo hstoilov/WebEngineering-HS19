18f5771c0f4bf9e142047414b2fb95277d6feb3e
Hristian <hristian.stoilov@students.fhnw.ch>
2019-10-07 09:22:05 +0200

    Change in 'feature/Calculator' made

---
+++ src/test/groovy/mvc/CalculatorControllerTest.groovy
@@ -0,0 +1,14 @@
+package mvc
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class CalculatorControllerTest extends Specification implements ControllerUnitTest<CalculatorController>{
+
+    void "simple avarage of #en and #exam should be #result"() {
+        when:
+        controller.calc(1.0, 2.0)
+        then:
+        model.result == 1.5
+    }
+}
